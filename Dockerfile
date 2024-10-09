e7a904b73bcd6fee4e008de141d61b2754f6fbf7
candelabp <109047221+candelabp@users.noreply.github.com>
2024-10-09 20:46:21 -0300

    dockerfile y test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
-FROM ubuntu:latest
-LABEL authors="cande"
+FROM alpine:latest as build
 
-ENTRYPOINT ["top", "-b"]
+RUN apk update
+RUN apk add openjdk17
+
+COPY . .
+RUN chmod +x ./gradlew
+RUN ./gradlew bootJar --no-daemon
+
+FROM openjdk:17-alpine
+EXPOSE 9000
+COPY --from=build ./build/libs/mutantesSpring-0.0.1-SNAPSHOT.jar ./app.jar
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/org/example/mutantesspring/MutantesSpringApplicationTests.java
+++ src/test/java/org/example/mutantesspring/MutantesSpringApplicationTests.java
@@ -1,13 +1,58 @@
 package org.example.mutantesspring;
 
+import org.example.mutantesspring.business.service.impl.DetectorMutanteService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class MutantesSpringApplicationTests {
-@Beforeall
 	@Test
-	void contextLoads() {
+	public void testEsMutanteConAdnMutante() {
+		String[] adnMutante = {
+				"ATGCGA",
+				"CAGTGC",
+				"TTATGT",
+				"AGAAGG",
+				"CCCCTA",
+				"TCACTG"
+		};
+
+		boolean resultado = DetectorMutanteService.esMutante(adnMutante);
+		assertTrue(resultado, "El ADN debería ser identificado como mutante");
 	}
 
-}
+	@Test
+	public void testEsMutanteConAdnNoMutante() {
+		String[] adnNoMutante = {
+				"ATGCGA",
+				"CAGTGC",
+				"TTATTT",
+				"AGACGG",
+				"GCGTCA",
+				"TCACTG"
+		};
+
+		boolean resultado = DetectorMutanteService.esMutante(adnNoMutante);
+		assertFalse(resultado, "El ADN no debería ser identificado como mutante");
+	}
+
+	@Test
+	public void testEsMutanteConAdnInvalido() {
+		String[] adnInvalido = {
+				"ATGXGA",
+				"CAGTGC",
+				"TTATTT",
+				"AGACGG",
+				"GCGTCA",
+				"TCACTG"
+		};
+
+		boolean resultado = DetectorMutanteService.caracteresPorCaracter(adnInvalido[0]);
+		assertFalse(resultado, "El ADN debería ser inválido por contener caracteres no permitidos");
+	}
+
+
+	}
